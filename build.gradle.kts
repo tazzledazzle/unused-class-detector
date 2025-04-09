93ae13930fe8d40e5d44b71657c8f839776b1d37
Terence Schumacher <terenceschumacher@gmail.com>
2025-04-09 12:00:25 -0700

    ! e added proper idea dependency; added maven dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,37 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 group = "com.tazzledazzle"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://maven.pkg.jetbrains.space/public/p/ij/intellij-plugin-service") // IntelliJ IDEA plugin repository
+    maven("https://www.jetbrains.com/intellij-repository/releases") // IntelliJ IDEA plugin repository
     mavenCentral()
-
-    intellijPlatform {
-    localPlatformArtifacts()
-
-//    version = "2023.1" // Specify the IntelliJ IDEA version
-//    plugins = ["java", "Kotlin"] // Add required plugins
-    }
+    maven ("https://oss.sonatype.org/content/repositories/snapshots/" )
 }
 
 val kotestVersion = "5.9.0"
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    implementation("com.jetbrains.intellij.platform:analysis-api:2019.1.4")
+    // IntelliJ IDEA dependencies
+    implementation("com.jetbrains.intellij.idea:ideaIC:2024.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+
+    // Maven dependencies
+    implementation ("org.apache.maven.shared:maven-dependency-analyzer:1.15.1")
+    implementation ("org.apache.maven:maven-project:3.9.6")
+    implementation ("org.apache.maven:maven-core:3.9.6")
+    implementation ("org.eclipse.aether:aether-api:1.1.0")
+    implementation ("org.eclipse.aether:aether-impl:1.1.0")
+    implementation ("org.eclipse.aether:aether-connector-basic:1.1.0")
+    implementation ("org.eclipse.aether:aether-transport-file:1.1.0")
+    implementation ("org.eclipse.aether:aether-transport-http:1.1.0")
+    // Test
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-
-    intellijPlatform {
-        intellijIdeaUltimate("2024.3.5")
-    }
 }
 
 tasks.test {
